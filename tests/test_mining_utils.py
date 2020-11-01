8e046cf38d55e539119ee55ed91881c1da7f55e0
DaviSegundo <davisp2009@hotmail.com>
2020-10-31 21:49:09 -0300

    test [#9]: Novos testes - T2

--- tests/test_mining_utils.py
+++ tests/test_mining_utils.py
@@ -1,25 +1,25 @@
-from lib.mining_utils import hasSrcExtension, isTest, isValidKey, filter_top_frequent_words, extractKeys
+from lib.mining_utils import has_Source_Extension, is_Test, is_Valid_Key, filter_top_frequent_words, extract_Keys
 import unittest
 
 
 
 class TestMiningUtils(unittest.TestCase):
-    def test_hasSrcExtension(self):
+    def test_has_Source_Extension(self):
         test_list = ["test.py", "test.c", "test.java", "test", "test.test"]
         for i in range(len(test_list)):
             if i < 3:
-                self.assertTrue(hasSrcExtension(test_list[i]))
+                self.assertTrue(has_Source_Extension(test_list[i]))
             else:
-                self.assertFalse(hasSrcExtension(test_list[i]))
+                self.assertFalse(has_Source_Extension(test_list[i]))
 
-    def test_isTest(self):
+    def test_is_Test(self):
         path_list = ["/src/test/test.py", "test/test.py", "/src/main.py"]
 
         for i in range(len(path_list)):
             if i < 2:
-                self.assertTrue(isTest(path_list[i]))
+                self.assertTrue(is_Test(path_list[i]))
             else:
-                self.assertFalse(isTest(path_list[i]))
+                self.assertFalse(is_Test(path_list[i]))
 
 
     def test_filter_top_frequent_words(self):
@@ -27,23 +27,23 @@ class TestMiningUtils(unittest.TestCase):
 
         self.assertEqual(filter_top_frequent_words(text), "home:4 sweet:2 alabama:1 place:1 like:1")
 
-    def test_extractKeys(self):
+    def test_extract_Keys(self):
         texto = 'CONTRIBUTED BY MUKUND THAKUR \
         HDFS-15253 DEFAULT CHECKPOINT TRANSFER SPEED, 50MB PER SECOND (#2366) \
         HDFS-15610 REDUCED DATANODE UPGRADE/HARDLINK THREAD FROM 12 TO 6 (#2365) \
         HADOOP-17021. ADD CONCAT FS COMMAND (#1993)'
 
-        self.assertEqual(extractKeys(texto), ['HDFS-15253', 'HDFS-15610', 'HADOOP-17021'])
+        self.assertEqual(extract_Keys(texto), ['HDFS-15253', 'HDFS-15610', 'HADOOP-17021'])
 
 
-    def test_isValidKey(self):
+    def test_is_Valid_Key(self):
         keys_list = ['HDFS-15613', 'HDFS-14442', 'HDFS-15543', '7345-chave', 'key123', 'test_key']
 
         for i in range(len(keys_list)):
             if i < 3:
-                self.assertTrue(isValidKey(keys_list[i]))
+                self.assertTrue(is_Valid_Key(keys_list[i]))
             else:
-                self.assertFalse(isValidKey(keys_list[i]))
+                self.assertFalse(is_Valid_Key(keys_list[i]))
 
 if __name__ == "__main__":
     unittest.main()

8a65c42392411deb9d6dc29195d1171ea7f42bd7
Diego Holanda <diegoholanda2008@hotmail.com>
2020-11-01 10:54:55 -0300

    build [#8]: Adiciona setup.py e README

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bugfixminer",
+    version="0.0.1",
+    author="Lincoln Rocha, Diego Freitas, Gabriel Freire, Pedro Ernesto, Davi Segundo, Eduardo Alcantra",
+    description="Mine bug reports using Jira and Git",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabrielfvale/engsoft-bugfixminer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.7',
+)
